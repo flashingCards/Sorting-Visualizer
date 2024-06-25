6c3ae2bc61d1774b03fb8acd6d34e8dbc4666044
flashingCards <141182783+flashingCards@users.noreply.github.com>
2024-06-25 21:27:14 +0530

    adding context folder with visualizer file to get started and base components of the website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    // https://sorting-visualizer-pied-nine.vercel.app/
     <div>
       sorting visualizer
     </div>
